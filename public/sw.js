dfe93cbde01528c5a080b69706868446bc7ced2c
Settimov7 <prytkov.n@yandex.ru>
2019-03-31 22:54:40 +0400

    исправил пути файлов

--- public/sw.js
+++ public/sw.js
@@ -2,27 +2,27 @@ self.addEventListener(`install`, (evt) => {
   const openCache = caches.open(`STATIC_V1.0`)
     .then((cache) => {
       return cache.addAll([
-        `/`,
-        `/css/`,
-        `/css/main.css`,
-        `/css/normalize.css`,
-        `/images/`,
-        `/images/posters/`,
-        `/images/posters/accused.jpg`,
-        `/images/posters/blackmail.jpg`,
-        `/images/posters/blue-blazes.jpg`,
-        `/images/posters/fuga-da-new-york.jpg`,
-        `/images/posters/moonrise.jpg`,
-        `/images/posters/three-friends.jpg`,
-        `/images/background.png`,
-        `/images/icon-favorite.png`,
-        `/images/icon-favorite.svg`,
-        `/images/icon-watched.png`,
-        `/images/icon-watched.svg`,
-        `/images/icon-watchlist.png`,
-        `/images/icon-watchlist.svg`,
-        `/index.html`,
-        `/bundle.js`
+        `./`,
+        `./css/`,
+        `./css/main.css`,
+        `./css/normalize.css`,
+        `./images/`,
+        `./images/posters/`,
+        `./images/posters/accused.jpg`,
+        `./images/posters/blackmail.jpg`,
+        `./images/posters/blue-blazes.jpg`,
+        `./images/posters/fuga-da-new-york.jpg`,
+        `./images/posters/moonrise.jpg`,
+        `./images/posters/three-friends.jpg`,
+        `./images/background.png`,
+        `./images/icon-favorite.png`,
+        `./images/icon-favorite.svg`,
+        `./images/icon-watched.png`,
+        `./images/icon-watched.svg`,
+        `./images/icon-watchlist.png`,
+        `./images/icon-watchlist.svg`,
+        `./index.html`,
+        `./bundle.js`
       ]);
     });
 
